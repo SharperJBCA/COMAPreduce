8bb78a10ac55e5d337b7eb5442261bd8debfffcb
SharperJBCA <heylookim@gmail.com>
2019-02-11 17:41:25 +0000

    Moved H5Data to baseclasses.py

--- comancpipeline/Analysis/BaseClasses.py
+++ comancpipeline/Analysis/BaseClasses.py
@@ -25,3 +25,130 @@ class DummyTest(DataStructure):
         h5data.update('new_test{}.h5'.format(h5data.rank))
         print(h5data.data.filename)
         h5data.data.create_dataset('a/b',(10,100,), dtype='f')
+
+
+class H5Data(object):
+    """
+    Main data structure for transporting COMAP TOD between pipeline operations.
+    Also keeps track of MPI information and sharing of data between cores.
+    """
+
+    def __init__(self, filename, rank, size , out_dir=None, mode='r'):
+        self.filename = filename
+        self.rank = rank
+        self.size = size
+        self.mode = mode
+        if isinstance(out_dir, type(None)):
+            self.out_dir = ''
+        else:
+            self.out_dir = out_dir
+
+        self.dsets= {}
+        self.hi = {}
+        self.lo = {}
+        self.fields = {}
+        self.selectAxes={}
+        self.splitAxis ={}
+        self.stop = False
+
+    def __del__(self):
+        del self.dsets
+        self.dsets = {}
+        self.fields = {}
+        self.hi = {}
+        self.lo = {}
+        self.fields = {}
+        self.data.close()
+        #self.output.close()
+
+    def setFields(self, fields, config):
+        """
+        Pre-allocate data splits for this core,
+        by reading in files from disk.
+        """
+        if isinstance(fields, type(None)):
+            return None
+        
+        for field, split in fields.items():
+            if not field in self.fields.keys(): # Has this data been read in already?
+                if split: # Are we splitting this data for MPI purposes?
+
+                    # We remove dimensions we don't care about...
+                    # (e.g. select just horn 1, sideband 1...
+                    slc = [slice(0,s,None) for s in self.data[field].shape]
+
+                    self.selectAxes[field], self.splitAxis[field] = Parser.parse_split(config, field)
+                    if not isinstance(self.selectAxes[field], type(None)):
+                        for i in self.selectAxes[field]:
+                            slc[i] = slice(i,i+1,None)
+
+                    self.lo[field], self.hi[field] = self.getDataRange(self.data[field].shape[self.splitAxis[field]])
+                    slc[self.splitAxis[field]] = slice(self.lo[field],self.hi[field])
+                    self.dsets[field] = self.data[field][tuple(slc)]
+                    #np.take(grpSliceObj, range(self.lo[field],self.hi[field]), axis=self.splitAxis[field]-len(self.selectAxes))
+                                                #[self.lo[field]:self.hi[field],...]
+                else:
+                    self.dsets[field] = self.data[field][...]
+                self.fields[field] = split
+
+    def outputFields(self):
+        """
+        Write the data back out to disk
+        """
+        for field, split in self.fields.items():
+            print(field, split)
+            tmp = self.output.create_dataset(field, self.dsets[field].shape)
+            print(tmp)
+            print(tmp.shape)
+            if split:
+
+                # We remove dimensions we don't care about...
+                # (e.g. select just horn 1, sideband 1...
+                #if not isinstance(self.selectAxes[field], type(None)):
+                #    for i in self.selectAxes[field]:
+                #        tmp = tmp[i]
+
+                self.lo[field], self.hi[field] = self.getDataRange(self.data[field].shape[self.splitAxis[field]])
+
+                slc = [slice(0,s,None) for s in self.dsets[field].shape]
+                slc[self.splitAxis[field]] = slice(self.lo[field],self.hi[field])
+                tmp[tuple(slc)] = self.dsets[field]
+            else:
+                if self.rank == 0:
+                    tmp[...] = self.dsets[field]
+
+    def setOutputAttr(self,grp, attr, value):
+        if not grp in self.output:
+            self.output.create_group(grp)
+
+        self.output[grp].attrs.create(attr, value)
+
+    def open(self, mode='a'):
+        self.mode = 'a'
+        self.data = h5py.File(self.filename,self.mode, driver='mpio', comm=comm)
+
+        # output datafile
+        #print('dsreduced/'+self.data.filename.split('/')[-1])
+        self.output = h5py.File(self.out_dir+'/'+self.data.filename.split('/')[-1],'a', driver='mpio', comm=comm)
+
+    def close(self):
+        self.outputFields()
+        self.__del__()
+
+    def update(self, filename, mode='a'):
+        self.close()
+        self.filename = filename
+        self.open(mode)
+
+    def getDataRange(self, N):
+        """
+        get the high/low range for a loop based
+        on the MPI rank/size and loop length
+        """
+        
+        allNodes = np.sort(np.mod(np.arange(N).astype(int), self.size))
+        thisNode = np.where((allNodes == self.rank))[0]
+
+        hi = int(np.max(thisNode)) + 1
+        lo = int(np.min(thisNode))
+        return lo, hi
--- run.py
+++ run.py
@@ -3,6 +3,7 @@ comm = MPI.COMM_WORLD
 
 import numpy as np
 #from comancpipeline.Tools import *
+from comancpipeline.Analysis.BaseClasses import H5Data
 from comancpipeline.Analysis import Calibration
 from comancpipeline.Tools import Parser
 import sys
@@ -15,146 +16,11 @@ default_parser.add_option("-f","--filename",dest='filename',action="store",type=
 default_parser.add_option("-P","--parameters",dest='parameters',action="store",type='str',default=None,help="Parameter file describing classes needed for data reduction.")
 
 
-class H5Data(object):
-
-    def __init__(self, filename, rank, size , out_dir=None, mode='r'):
-        self.filename = filename
-        self.rank = rank
-        self.size = size
-        self.mode = mode
-        if isinstance(out_dir, type(None)):
-            self.out_dir = ''
-        else:
-            self.out_dir = out_dir
-
-        self.dsets= {}
-        self.hi = {}
-        self.lo = {}
-        self.fields = {}
-        self.selectAxes={}
-        self.splitAxis ={}
-        self.stop = False
-
-    def __del__(self):
-        del self.dsets
-        self.dsets = {}
-        self.fields = {}
-        self.hi = {}
-        self.lo = {}
-        self.fields = {}
-        self.data.close()
-        #self.output.close()
-
-    def setFields(self, fields, config):
-        """
-        Pre-allocate data splits for this core,
-        by reading in files from disk.
-        """
-        if isinstance(fields, type(None)):
-            return None
-        
-        for field, split in fields.items():
-            if not field in self.fields.keys(): # Has this data been read in already?
-                if split: # Are we splitting this data for MPI purposes?
-
-                    # We remove dimensions we don't care about...
-                    # (e.g. select just horn 1, sideband 1...
-                    slc = [slice(0,s,None) for s in self.data[field].shape]
-
-                    self.selectAxes[field], self.splitAxis[field] = Parser.parse_split(config, field)
-                    if not isinstance(self.selectAxes[field], type(None)):
-                        for i in self.selectAxes[field]:
-                            slc[i] = slice(i,i+1,None)
-
-                    self.lo[field], self.hi[field] = self.getDataRange(self.data[field].shape[self.splitAxis[field]])
-                    slc[self.splitAxis[field]] = slice(self.lo[field],self.hi[field])
-                    self.dsets[field] = self.data[field][tuple(slc)]
-                    #np.take(grpSliceObj, range(self.lo[field],self.hi[field]), axis=self.splitAxis[field]-len(self.selectAxes))
-                                                #[self.lo[field]:self.hi[field],...]
-                else:
-                    self.dsets[field] = self.data[field][...]
-                self.fields[field] = split
-
-    def outputFields(self):
-        """
-        Write the data back out to disk
-        """
-        for field, split in self.fields.items():
-            print(field, split)
-            tmp = self.output.create_dataset(field, self.dsets[field].shape)
-            print(tmp)
-            print(tmp.shape)
-            if split:
-
-                # We remove dimensions we don't care about...
-                # (e.g. select just horn 1, sideband 1...
-                #if not isinstance(self.selectAxes[field], type(None)):
-                #    for i in self.selectAxes[field]:
-                #        tmp = tmp[i]
-
-                self.lo[field], self.hi[field] = self.getDataRange(self.data[field].shape[self.splitAxis[field]])
-
-                slc = [slice(0,s,None) for s in self.dsets[field].shape]
-                slc[self.splitAxis[field]] = slice(self.lo[field],self.hi[field])
-                tmp[tuple(slc)] = self.dsets[field]
-            else:
-                if self.rank == 0:
-                    tmp[...] = self.dsets[field]
-
-    def setOutputAttr(self,grp, attr, value):
-        if not grp in self.output:
-            self.output.create_group(grp)
-
-        self.output[grp].attrs.create(attr, value)
-
-    def open(self, mode='a'):
-        self.mode = 'a'
-        self.data = h5py.File(self.filename,self.mode, driver='mpio', comm=comm)
-
-        # output datafile
-        #print('dsreduced/'+self.data.filename.split('/')[-1])
-        self.output = h5py.File(self.out_dir+'/'+self.data.filename.split('/')[-1],'a', driver='mpio', comm=comm)
-
-    def close(self):
-        self.outputFields()
-        self.__del__()
-
-    def update(self, filename, mode='a'):
-        self.close()
-        self.filename = filename
-        self.open(mode)
-
-    def getDataRange(self, N):
-        """
-        get the high/low range for a loop based
-        on the MPI rank/size and loop length
-        """
-        
-        allNodes = np.sort(np.mod(np.arange(N).astype(int), self.size))
-        thisNode = np.where((allNodes == self.rank))[0]
-
-        hi = int(np.max(thisNode)) + 1
-        lo = int(np.min(thisNode))
-        return lo, hi
-
-
-def getDataRange(rank, size, N):
-    """
-    get the high/low range for a loop based
-    on the MPI rank/size and loop length
-    """
-        
-    allNodes = np.sort(np.mod(np.arange(N).astype(int), size))
-    thisNode = np.where((allNodes == rank))[0]
-
-    hi = int(np.max(thisNode)) + 1
-    lo = int(np.min(thisNode))
-    return range(lo, hi)
-
 import time
 
 def main(args):
     
+    # Read in the metadata for this pipeline run
     options,pargs = default_parser.parse_args(args)
 
     selectors, targets, config = Parser.parse_parameters(options.parameters)
@@ -164,33 +30,38 @@ def main(args):
         filelist = np.loadtxt(options.filelist, dtype=str,ndmin=1)
 
     
+    # Start main loop over each file
     for i in range(filelist.size):
         if comm.rank == 0:
             start = time.time()
 
         filename = filelist[i]
         print('Opening: {}'.format(filename.split('/')[-1]))
-        try:
+        try: # If the file fails to open we just continue to the next...
+
             h5data = H5Data(filename,comm.rank, comm.size, out_dir=config.get('Inputs', 'out_dir'))
-            print(filename)
             h5data.open()
-            try:
+
+            try: # Check to see if this file has a comment string, no string then no analysis!
                 comment = (h5data.data['comap'].attrs['comment'].lower()).decode('utf-8')
             except KeyError:
                 print('No comment, skipping...')
                 continue
 
+            # Does the comment contain a target we are wanting to analyse?
             check = [t in comment for t in targets]
-            print(comment, targets)
             if not any(check):
                 print('{} does not contain allowed target'.format(comment))
                 continue
 
+            # Loop over pipeline operations
             for selector in selectors:
+
+                # Update stored data storage if required...
                 h5data.setFields(selector.fields, config) # which fields does this operator need?
                 print(selector)
                 selector(h5data)
-                try:
+                try: # Run any plotting functions if there are any...
                     selector.plot() # make any plots
                 except AttributeError:
                     pass
