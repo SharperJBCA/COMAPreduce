Fixed function "getSourcePosition", for Planet data was returning radians when degrees expected. Updated simulations package to generate diffuse data for creating mock continuum data.

comancpipeline/Analysis/Simulations.py
@@ -23,8 +23,56 @@ comm = MPI.COMM_WORLD
 import os
 import healpy as hp
 import scipy.fftpack as sfft
+from scipy import linalg as la
 
-class SimulateSource(DataStructure):
+from tqdm import tqdm
+
+# Class for storing source locations
+class Source:
+    def __init__(self,x,y, hmsMode=True):
+        """
+        If hmsMode = True (default) then x and y are 3 element lists containing (HH,MM,SS.S) and (DD,MM,SS.S) respectively.
+        else just pass the degrees of each coordinates
+        """
+        
+        if hmsMode:
+            self.x = self.HMS2Degree(x)
+            self.y = self.DMS2Degree(y)
+        else:
+            self.x = x
+            self.y = y
+
+    def __call__(self):
+        return self.x, self.y
+
+    def amplitude(self, frequency, fwhm):
+        a = 4.695
+        b = 0.085
+        c = -0.178
+        logS = a + b*np.log10(frequency*1e3) + c*np.log10(frequency*1e3)**2
+        S = 10**logS
+
+        c = 3e8
+        kb = 1.38e-23
+        T2Jy = 2 * kb * 1e9**2 / c**2 * frequency**2
+
+        return  S / T2Jy / 1.13/ (fwhm*np.pi/180.)**2 * 1e-26
+    
+    def DMS2Degree(self,d):
+        """
+        Convert DD:MM:SS.S format to degrees
+        """
+        return d[0] + d[1]/60. + d[2]/60.**2
+    
+    def HMS2Degree(self,d):
+        return self.DMS2Degree(d)*15
+
+sources = {'TauA':Source([5,34,31.94],[22,0,52.2]),
+           'CasA':Source([23,23,24.0],[58,48,54.0]),
+           'CygA':Source([19,59,28.36],[40,44,2.10])}
+
+
+class SimulateObservation(DataStructure):
     """
     Base source fitting class.
 
@@ -33,28 +81,194 @@ class SimulateSource(DataStructure):
     Beam fitting functions.
     """
 
-    def __init__(self,a=1., x0=0, y0=0, sx=4./60., sy=4./60., phi=0., offset=0, gx=0, gy=0, xoff=0, yoff=0 ):
-        self.P = [a, xoff/60., sx, yoff/60., sy, phi*np.pi/180., offset, gx, gy]
-        self.x0, self.y0 = x0, y0
+    def __init__(self,outputdir='Simulations/', source='CygA' ):
+        self.source = sources[source]
+        self.outputdir = outputdir
+
+        self.fwhm = 4.5/60. 
+        
+        self.tau = 0.1
+        self.Tatm= 300
+
+        self.sampleRate = 50.
+        self.beta = 1e9
+
+        self.noisecov = np.loadtxt('/scratch/nas_comap1/tjrennie/Stuart/OBS:8502CORMATRIX:BAND0',delimiter=',')
+        print(self.noisecov.shape)
+
+    def corrnoise(self,N):
+        L,lu1,lu2 = la.lu(self.noisecov)
+        z = np.random.normal(size=(17,N))
+
+        def fnoise(tod):
+            ftod = sfft.fft(tod,axis=1)
+            f = np.fft.fftfreq(tod.shape[1],d=1./self.sampleRate)
+            f[0] = f[1]
+            P = np.abs(f)**-1.5
+            P /= np.sum(P)
+            ftod = ftod*np.sqrt(P)[np.newaxis,:]
+            return np.real(sfft.ifft(ftod,axis=1))
+
+        z = fnoise(z)
+        return lu1.dot(z)
+
     def __str__(self):
         return 'Simulating source at {:.2f} {:.2f}'.format(self.x0, self.y0)
 
     def run(self,data):
 
         self.simulate(data)
+        #self.write(data)
+
+    def Gauss2D(self,Amplitude, xcen, ycen, sigma,x , y):
+        
+        Z = (x-xcen)**2/sigma**2 + (y-ycen)**2/sigma**2
+        return Amplitude * np.exp(-0.5*Z)
+
+    def Atmos(self, el):
+        return self.Tatm*(1-np.exp(-self.tau/np.sin(el*np.pi/180.)))
+
+    def ReceiverNoise(self,tod):
+        sigma = tod/np.sqrt(self.beta/self.sampleRate)
+        return np.random.normal(scale=sigma)
+        
 
     def simulate(self,data):
-        tod = data.getdset('spectrometer/tod')
-        ra  = data.getdset('spectrometer/pixel_pointing/pixel_ra')
-        dec = data.getdset('spectrometer/pixel_pointing/pixel_dec')
-        nHorns, nSBs, nChans, nSamples = tod.shape
-        rot = hp.rotator.Rotator(rot=[self.x0, self.y0])
-        for i in range(nHorns):
-            decRot, raRot = rot((90-dec[i,:])*np.pi/180., ra[i,:]*np.pi/180.)
-            for j in range(nSBs):
-                for k in range(nChans):
-                    tod[i,j,k,:] += Fitting.Gauss2dRotPlane(self.P, raRot*180./np.pi, (np.pi/2.-decRot)*180./np.pi,0,0)
+        todshape = data['spectrometer/band_average'].shape
+        self.feeds = data['spectrometer/feeds'][:]
+        self.mjd = data['spectrometer/MJD'][:]
+        self.az  = data['spectrometer/pixel_pointing/pixel_az'][...]
+        self.el  = data['spectrometer/pixel_pointing/pixel_el'][...]
+        self.ra  = data['spectrometer/pixel_pointing/pixel_ra'][...]
+        self.dec = data['spectrometer/pixel_pointing/pixel_dec'][...]
 
+        self.tod = np.zeros(todshape)
+
+        self.source.ra,self.source.dec = Coordinates.precess2year(np.array([self.source.x]),
+                                                                  np.array([self.source.y]),
+                                                                  np.array([np.mean(self.mjd)]))
+
+        nHorns, nSBs, nSamples = todshape
+        rot = hp.rotator.Rotator(rot=[self.source.ra, self.source.dec])
+        for j in range(nSBs):
+            for i in tqdm(range(nHorns)):
+                decRot, raRot = rot((90-self.dec[i,:])*np.pi/180., self.ra[i,:]*np.pi/180.)
+                self.tod[i,j,:] += self.Gauss2D(self.source.amplitude(30, self.fwhm),
+                                           0,0,self.fwhm/2.355,
+                                           raRot*180./np.pi, (np.pi/2.-decRot)*180./np.pi)
+
+                self.tod[i,j,:] += self.Atmos(self.el[i,:])
+                self.tod[i,j,:] += self.ReceiverNoise(self.tod[i,j,:])
+            ft = self.corrnoise(self.tod.shape[-1])*5
+            self.tod[:17,j,:] += ft
+
+    def write(self,data):
+
+        filename = data.filename.split('/')[-1]
+        out = h5py.File('{}/{}'.format(self.outputdir,filename))
+
+        out.create_dataset('spectrometer/band_average',data=self.tod)
+        out.create_dataset('spectrometer/MJD',data=self.mjd)
+        out.create_dataset('spectrometer/feeds',data=self.feeds)
+        out.create_dataset('spectrometer/features',data=data['spectrometer/features'][...])
+        out.create_dataset('spectrometer/pixel_pointing/pixel_az',data=self.az)
+        out.create_dataset('spectrometer/pixel_pointing/pixel_el',data=self.el)
+        out.create_dataset('spectrometer/pixel_pointing/pixel_ra',data=self.ra)
+        out.create_dataset('spectrometer/pixel_pointing/pixel_dec',data=self.dec)
+        out.create_group('comap')
+        for attr,value in data['comap'].attrs.items():
+            out['comap'].attrs[attr] = value
+        out.close()
+
+##
+from astropy.io import fits
+class SimulateDiffuse(SimulateObservation):
+    """
+    Base source fitting class.
+
+    Contains functions for rotating coordinate system to frame
+    of the source being fitted. Useful for aperture photometry and
+    Beam fitting functions.
+    """
+
+    def __init__(self,outputdir='Simulations/', source='CygA' ):
+        self.source = sources[source]
+        self.outputdir = outputdir
+
+        self.fwhm = 4.5/60. 
+        
+        self.tau = 0.1
+        self.Tatm= 300
+
+        self.sampleRate = 50.
+        self.beta = 1e9
+
+        self.noisecov = np.loadtxt('/scratch/nas_comap1/tjrennie/Stuart/OBS:8502CORMATRIX:BAND0',delimiter=',')
+        self.Trec = 20
+
+        # Get the VGPS data
+        MJysr2K = 20e-6
+        self.skymap = hp.read_map('/local/scratch/sharper/TemplateFittingNew/MAPS/IRIS_combined_SFD_really_nohole_nosource_4_2048.fits')*MJysr2K
+
+    def VGPS(self,ra, dec):
+        rot = hp.rotator.Rotator(coord=['C','G'])
+        gb, gl = rot((90-dec)*np.pi/180., ra*np.pi/180.)
+        gb, gl = (np.pi/2. - gb)*180./np.pi, gl*180./np.pi
+
+        xpix, ypix = np.meshgrid(np.arange(self.vgps.shape[1]),np.arange(self.vgps.shape[0]))
+
+        crval = self.vgps_wcs.crpix[0]*self.vgps_wcs.cdelt[0],self.vgps_wcs.crpix[1]*self.vgps_wcs.cdelt[1] 
+        print(crval)
+
+
+        glpix, gbpix = self.vgps_wcs.all_pix2world(xpix.flatten(),ypix.flatten(),0)
+        print(self.vgps_wcs.naxis,self.vgps_wcs.cdelt,self.vgps_wcs.crval,self.vgps_wcs.crpix,self.vgps_wcs.ctype)
+        print( self.vgps_wcs.all_pix2world(7113,513,0))
+        #pyplot.plot(gl,gb,'.')
+        pyplot.plot(glpix,gbpix,',')
+        pyplot.show()     
+
+    def Diffuse(self,ra, dec):
+        rot = hp.rotator.Rotator(coord=['C','G'])
+        gb, gl = rot((90-dec)*np.pi/180., ra*np.pi/180.)
+        #gb, gl = (np.pi/2. - gb)*180./np.pi, gl*180./np.pi
+       # pix = hp.ang2pix(2048,gb,gl)
+        
+        return hp.get_interp_val(self.skymap, gb,gl) #self.skymap[pix]
+
+    def simulate(self,data):
+        todshape = data['spectrometer/band_average'].shape
+        self.feeds = data['spectrometer/feeds'][:]
+        self.mjd = data['spectrometer/MJD'][:]
+        self.az  = data['spectrometer/pixel_pointing/pixel_az'][...]
+        self.el  = data['spectrometer/pixel_pointing/pixel_el'][...]
+        self.ra  = data['spectrometer/pixel_pointing/pixel_ra'][...]
+        self.dec = data['spectrometer/pixel_pointing/pixel_dec'][...]
+
+        self.tod = np.zeros(todshape)
+
+        self.source.ra,self.source.dec = Coordinates.precess2year(np.array([self.source.x]),
+                                                                  np.array([self.source.y]),
+                                                                  np.array([np.mean(self.mjd)]))
+
+        nHorns, nSBs, nSamples = todshape
+        rot = hp.rotator.Rotator(rot=[self.source.ra, self.source.dec])
+        for j in range(nSBs):
+            for i in tqdm(range(nHorns)):
+                decRot, raRot = rot((90-self.dec[i,:])*np.pi/180., self.ra[i,:]*np.pi/180.)
+                #self.tod[i,j,:] += self.Gauss2D(self.source.amplitude(30, self.fwhm),
+                #                           0,0,self.fwhm/2.355,
+                #                           raRot*180./np.pi, (np.pi/2.-decRot)*180./np.pi)
+
+                self.tod[i,j,:] += self.Diffuse(self.ra[i,:],self.dec[i,:])
+                self.tod[i,j,:] += self.Trec
+                #self.tod[i,j,:] += self.Atmos(self.el[i,:])
+                #self.tod[i,j,:] += self.ReceiverNoise(self.tod[i,j,:])
+            ft = self.corrnoise(self.tod.shape[-1])/10
+            self.tod[:17,j,:] += ft
+            pyplot.plot(self.tod[0,0,:]-np.nanmedian(self.tod[0,0,:]))
+            pyplot.plot(self.Diffuse(self.ra[0,:],self.dec[0,:]))
+            pyplot.show()
 
 class SimulateNoise(DataStructure):
     """

comancpipeline/Analysis/SourceFitting.py
@@ -167,12 +167,16 @@ class FitSource(DataStructure):
     Beam fitting functions.
     """
 
-    def __init__(self, output_dir='', nworkers= 1, average_width=512,calvanedir='AncillaryData/CalVanes',
+    def __init__(self, feeds='all', output_dir='', nworkers= 1, average_width=512,calvanedir='AncillaryData/CalVanes',
                  x0=0, y0=0, lon=-118.2941, lat=37.2314, filtertod=False):
         """
         nworkers - how many threads to use to parallise the fitting loop
         average_width - how many channels to average over
         """
+
+        self.feeds = feeds
+        
+
         self.x0 = x0
         self.y0 = y0
         self.lon = lon
@@ -200,7 +204,16 @@ class FitSource(DataStructure):
         # Get data structures we need
         alltod = data['spectrometer/tod']
         btod = data['spectrometer/band_average']
-        feeds = data['spectrometer/feeds'][:]
+        if self.feeds == 'all':
+            feeds = data['spectrometer/feeds'][:]
+        else:
+            if not isinstance(self.feeds,list):
+                self.feeds = [int(self.feeds)]
+                feeds = self.feeds
+            else:
+                feeds = [int(f) for f in self.feeds]
+
+        self.feed = feeds
         mjd = data['spectrometer/MJD'][:]
         az  = data['spectrometer/pixel_pointing/pixel_az'][:]
         el  = data['spectrometer/pixel_pointing/pixel_el'][:]
@@ -216,49 +229,29 @@ class FitSource(DataStructure):
             return
 
         nHorns, nSBs, nChans, nSamples = alltod.shape
+        nHorns = len(feeds)
 
         # --- Average down the data
-        calvane = pd.read_pickle('{}/{}_TsysGainRMS.pkl'.format(self.calvanedir, data.filename.split('/')[-1].split('.hd5')[0]))
-        idx = pd.IndexSlice
         width = self.average_width
         tod = np.zeros((nHorns, nSBs, nChans//width, nSamples))
         nHorns, nSBs, nChans, nSamples = tod.shape
-
-        # --- Tuples of values to loop over
-        # loopvals = []
-        # for horn in range(nHorns):
-        #     for sb in range(nSBs):
-        #         for chan in range(nChans):
-        #             loopvals += [(horn,feeds[horn],sb,chan)]
-
-        # def AvgFunc(alltod, width, loopvals):
-        #     horn, feed, sb, chan = loopvals
-
-        #     weights = 1./calvane.loc(axis=0)[idx[:,:,'RMS',feed,sb]].values.flatten().astype(float)**2#.groupby(level=['Date']).mean().values
-        #     weights[np.isinf(weights)] = 0
-        #     gain = calvane.loc(axis=0)[idx[:,:,'Gain',feed,sb]].values.flatten().astype(float)#.groupby(level=['Date']).mean().values
-        #     try:
-        #         bot = np.nansum(weights[chan*width:(chan+1)*width])
-        #     except:
-        #         return None
-        #     tod = np.sum(alltod[horn,sb,chan*width:(chan+1)*width,:]*weights[chan*width:(chan+1)*width,np.newaxis],axis=0)/bot
-        #     gainAvg = np.nansum(gain[chan*width:(chan+1)*width]*weights[chan*width:(chan+1)*width])/bot 
-        #     tod[horn,sb,chan,:] /= gainAvg
-
-        # AvgFuncPartial = Partial(AvgFunc, alltod, width)
-
-        # # --- Parallelise the fitting procedure for each channel
-        # with concurrent.futures.ProcessPoolExecutor(max_workers=self.nworkers) as executor:
-        #     for output in executor.map(AvgFuncPartial, loopvals, chunksize=len(loopvals)//self.nworkers//2):
-        #         if not isinstance(output, type(None)):
-        #             tod_out,horn,sb,chan = output
-        #             tod[horn,sb,chan,:] = tod_out
+        nHorns = len(feeds)
+        try:
+            calvane = pd.read_pickle('{}/{}_TsysGainRMS.pkl'.format(self.calvanedir, data.filename.split('/')[-1].split('.hd5')[0]))
+            idx = pd.IndexSlice
+            weights = 1./calvane.loc(axis=0)[idx[:,:,'RMS',:,:]].values.flatten().astype(float)**2
+            gain = calvane.loc(axis=0)[idx[:,:,'Gain',feed,sb]].values.flatten().astype(float)
+            #weights = np.reshape(weights,(
+        except:
+            print('No calibration file found, assuming equal weights')
+            weights = np.ones(nChans*width)
+            weights[:5] = 0
+            weights[:-5] = 0
+            gain = np.ones(nChans*width)
 
         for horn, feed in zip(range(nHorns),feeds):
             for sb in tqdm(range(nSBs)):
-                weights = 1./calvane.loc(axis=0)[idx[:,:,'RMS',feed,sb]].values.flatten().astype(float)**2#.groupby(level=['Date']).mean().values
                 weights[np.isinf(weights)] = 0
-                gain = calvane.loc(axis=0)[idx[:,:,'Gain',feed,sb]].values.flatten().astype(float)#.groupby(level=['Date']).mean().values
                 for chan in range(nChans):
                     try:
                         bot = np.nansum(weights[chan*width:(chan+1)*width])
@@ -336,6 +329,7 @@ class FitSource(DataStructure):
         Write the Tsys, Gain and RMS to a pandas data frame for each hdf5 file.
         """        
         nHorns, nSBs, nChan, nSamps = data['spectrometer/tod'].shape
+        nHorns = len(self.feeds)
         nChan = int(nChan//self.average_width) # need to capture the fact the data is averaged
         nParams = 9
 
@@ -348,7 +342,8 @@ class FitSource(DataStructure):
         startDate = Types.Filename2DateTime(data.filename)
 
         # Reorder the data
-        horns = data['spectrometer/feeds'][:]
+        
+        horns = self.feeds
         sidebands = np.arange(nSBs).astype(int)
         channels  = np.arange(nChan).astype(int)
         modes = ['Fits', 'Errors']
@@ -434,6 +429,8 @@ class FitSource(DataStructure):
                     pyplot.clf()
 
 
+
+# ---  OLD
 
 class FitSourceAlternateScans(FitSource):
     def __init__(self, beammodel = 'JamesBeam', x0=0, y0=0, lon=-118.2941, lat=37.2314, filtertod=False):